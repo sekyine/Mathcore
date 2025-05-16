1785a589df552fa9fdebb708ae3df46c3df2dcd5
sekyine <miketyukuba@gmail.com>
2025-05-16 16:52:27 +0900

    ユーザー認証機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get "static_pages/home"
 
   get "up" => "rails/health#show", as: :rails_health_check
 
   root 'static_pages#home'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
 end
