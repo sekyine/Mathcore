7da1bec9361fb7dd88308141ff521f869a5c53bd
Yusuke Tan <yousuke0402@gmail.com>
2025-06-11 14:52:57 +0900

    secondcommt

--- db/schema.rb
+++ db/schema.rb
@@ -49,17 +49,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_06_071507) do
     t.index ["user_id"], name: "index_user_cards_on_user_id"
   end
 
-
-  create_table "usercards", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "card_id", null: false
-    t.integer "quantity", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["card_id"], name: "index_usercards_on_card_id"
-    t.index ["user_id"], name: "index_usercards_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "nickname"
