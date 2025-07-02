e84930755e7c0d425afaa12fc2710d29c1c4fa57
ATOMU1147 <s2513864@u.tsukuba.ac.jp>
2025-07-02 14:22:29 +0900

    import

--- db/schema.rb
+++ db/schema.rb
@@ -47,6 +47,25 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_041255) do
     t.string "effect_type"
     t.integer "power"
     t.string "question"
+    t.string "answer"
+  end
+
+  create_table "deck_cards", force: :cascade do |t|
+    t.integer "deck_id", null: false
+    t.integer "card_id", null: false
+    t.integer "position"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id"], name: "index_deck_cards_on_card_id"
+    t.index ["deck_id"], name: "index_deck_cards_on_deck_id"
+  end
+
+  create_table "decks", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_decks_on_user_id"
   end
 
   create_table "dungeons", force: :cascade do |t|
@@ -86,6 +105,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_02_041255) do
 
   add_foreign_key "battle_investigates", "users"
   add_foreign_key "battles", "users"
+  add_foreign_key "deck_cards", "cards"
+  add_foreign_key "deck_cards", "decks"
+  add_foreign_key "decks", "users"
   add_foreign_key "solved_cards", "cards"
   add_foreign_key "solved_cards", "users"
   add_foreign_key "user_cards", "cards"
