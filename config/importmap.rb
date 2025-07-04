3759ac8ab2edbdad7c9b373fcd819d644a828918
Yusuke Tan <yousuke0402@gmail.com>
2025-07-04 17:45:57 +0900

    MUSIC

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,10 +1,12 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-pin "bootstrap" # @5.3.7
-pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
+# BootstrapとPopper.jsをバンドル版でCDNから読み込みます
+pin "bootstrap", to: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+
+# application.jsからimportしている他のファイルをpinします
+pin "randomizer", to: "randomizer.js"
+pin "gacha_animation", to: "gacha_animation.js"
