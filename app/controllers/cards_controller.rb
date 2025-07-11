ff1a8497883f336b1d95f94a363f648413da3023
sekyine <miketyukuba@gmail.com>
2025-07-11 16:12:53 +0900

    全然関係ない部分だけどカード一覧の問題に正答した場合に出てくる(余計な)メッセージを削除

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -5,9 +5,9 @@ class CardsController < ApplicationController
 
     if correct
       SolvedCard.find_or_create_by!(user: current_user, card: card)
-      flash[:notice] = "正解！カード「#{card.bunya}」を解放しました。"
+      # flash[:notice] = "正解！カード「#{card.bunya}」を解放しました。"
     else
-      flash[:notice] = "不正解…また挑戦してね！"
+      # flash[:notice] = "不正解…また挑戦してね！"
     end
 
     # redirect_back(fallback_location: root_path)
